1e39272e637ec7dfb3a06d60eca1f71b3b11282d
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-09-05 00:34:44 -0500

    creacion de una lista estatica en el home y se cambio de tema a la app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,12 +7,19 @@ import { LugarPage } from '../lugar/lugar';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  lugares: any=[
+    {nombre:'Leonardo', direccion:'Catamayo', categoria:'Categoria 1'},
+    {nombre:'Daniel', direccion:'Catamayo', categoria:'Categoria 1'},
+    {nombre:'Maria', direccion:'Catamayo', categoria:'Categoria 1'}
+  ];
   constructor(public navCtrl: NavController) {
 
   }
   navegarALugar(name){
     //name es el nombre de la vista, depende del nombre de la vista donde se presione
     this.navCtrl.push(LugarPage, {nombre:name});//el parametro se llama nombre, el cual va ser obtenido en lugar.ts
+  }
+  irVistaDetalle(){
+    alert('Hola');
   }
 }
