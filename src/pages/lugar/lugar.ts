1b5cb56da1c1874e827961ffd45611155df55d82
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-09-09 15:38:27 -0500

    Reto: Se añadio una imagen al formulario, se guardo en la DB de FB, y se hizo la presentacion en el home

--- src/pages/lugar/lugar.ts
+++ src/pages/lugar/lugar.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 //import { TerceraPage } from '../tercera/tercera';
 import { LugaresService } from '../../services/lugares.service';
+import { Functions } from './functions';
 
 /**
  * Generated class for the LugarPage page.
@@ -17,11 +18,21 @@ import { LugaresService } from '../../services/lugares.service';
 })
 export class LugarPage {
   lugar:any={};
+  file:any=[];
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public lugaresService:LugaresService) {
     this.lugar = navParams.get('lugar'); //recoge los parametros enviados desde el home
   }
+  uploadPhoto($event):void{
+    this.file = $event.target.files[0];
 
+    Functions.getBase64(this.file).then((result)=>{
+      console.log(result);
+      this.lugar.imagen= result;
+      return result;
+    });
+
+  }
   guardarLugar(){
     if(!this.lugar.id){//si no trae el id, le asignamos
       this.lugar.id = Date.now();
